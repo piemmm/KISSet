d0922faa0c148324b3dc060d872528ec251ac811
piemmm <ian@prowl.org>
2023-07-28 22:36:28 +0100

    use sendFrame for the moment

--- src/main/java/org/prowl/kisset/ax25/AX25OutputStream.java
+++ src/main/java/org/prowl/kisset/ax25/AX25OutputStream.java
@@ -192,8 +192,8 @@ class AX25OutputStream extends OutputStream {
                         f.setNS(nextVS);
                         f.setNR(connState.vr);
                         LOG.debug("sending I frame " + f.sender + "->" + f.dest + " NS=" + f.getNS() + " NR=" + f.getNR() + " #=" + f.body.length);
-                        connState.stack.getTransmitting().queue(f);
-                        //connState.connector.sendFrame(f);
+                        //connState.stack.getTransmitting().queue(f);
+                        connState.connector.sendFrame(f);
                         connState.setResendableFrame(f, connState.stack.getTransmitting().getRetransmitCount()); // Make sure we resend it if we have no ack
                     } else {
                         throw new NullPointerException("no TransmittingConnector to send data through");
