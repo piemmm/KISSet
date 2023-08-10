130b48b869c1845ba1b777e4bab8944f6363a126
piemmm <ian@prowl.org>
2023-08-10 11:35:25 +0100

    wrap exception

--- src/main/java/org/prowl/kisset/netrom/RoutingListener.java
+++ src/main/java/org/prowl/kisset/netrom/RoutingListener.java
@@ -1,6 +1,8 @@
 package org.prowl.kisset.netrom;
 
 import com.google.common.eventbus.Subscribe;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.prowl.kisset.ax25.AX25Frame;
 import org.prowl.kisset.eventbus.SingleThreadBus;
 import org.prowl.kisset.eventbus.events.HeardNodeEvent;
@@ -12,15 +14,22 @@ public enum RoutingListener {
 
     INSTANCE;
 
+    private static final Log LOG = LogFactory.getLog("RoutingListener");
+
+
     RoutingListener() {
         SingleThreadBus.INSTANCE.register(this);
     }
 
     @Subscribe
     public void onHeardNode(HeardNodeEvent event) {
-        if (event.getNode().getFrame().getPid() == AX25Frame.PID_NETROM) {
-            NetROMRoutingPacket netROMRoutingPacket = new NetROMRoutingPacket(event.getNode());
-            RoutingTable.INSTANCE.addNodes(netROMRoutingPacket.getNodesInThisPacket());
+        try {
+            if (event.getNode().getFrame().getPid() == AX25Frame.PID_NETROM) {
+                NetROMRoutingPacket netROMRoutingPacket = new NetROMRoutingPacket(event.getNode());
+                RoutingTable.INSTANCE.addNodes(netROMRoutingPacket.getNodesInThisPacket());
+            }
+        } catch(Throwable e) {
+            LOG.error(e.getMessage(),e);
         }
     }
 }
